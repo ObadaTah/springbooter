1ef5c549143b06d661d45604b44af4f924b9ad36
ObadaTah <obadatahboub20@gmail.com>
2024-04-15 12:46:42 +0200

    added gitignore

--- generator.py
+++ generator.py
@@ -1,17 +1,82 @@
 import generateClass, generateController, generateModelAssembler, generateNotFoundAdvice, generateRepository, generateNotFoundException
 import os
 
+# classes = [
+#     {
+#         "className": "Academic",
+#         "attr": {
+#             "badge": "String",
+#             "education": "String",
+#             "organizationId": "User",
+#             "position": "Position",
+#         },
+#     },
+#     {"className": "Organization", "attr": {"type": "String", "verified": "Boolean"}},
+#     {
+#         "className": "Post",
+#         "attr": {
+#             "postId": "Long",
+#             "content": "String",
+#             "visibility": "Visibility",
+#             "publisher": "User",
+#             "interactionCount": "Integer",
+#             "opinionsCount": "Integer",
+#             "reShare": "Post",
+#         },
+#     },
+#     {"className": "Article", "attr": {"content": "String"}},
+#     {
+#         "className": "ResearchPaper",
+#         "attr": {
+#             "description": "String",
+#             "validatedBy": "Organization",
+#             "subject": "String",
+#             "language": "String",
+#             "noOfPages": "Integer",
+#         },
+#     },
+#     {
+#         "className": "Media",
+#         "attr": {"mediaId": "Long", "type": "MediaType", "path": "String"},
+#     },
+#     {
+#         "className": "Interaction",
+#         "attr": {
+#             "interactionId": "Long",
+#             "type": "InteractionType",
+#             "timestamp": "datetime",
+#         },
+#     },
+#     {"className": "Opinion", "attr": {"opinionId": "Long", "content": "Text"}},
+#     {
+#         "className": "Notification",
+#         "attr": {
+#             "notificationId": "Long",
+#             "content": "String",
+#             "status": "Status",
+#             "timestamp": "datetime",
+#         },
+#     },
+# ]
+
 classes = [
     {
-        "className": "Payment",
-        "attr": {"amount": "double", "date": "LocalDate", "type": "PaymentType"},
-    },
-    {
-        "className": "Patient",
-        "attr": {"name": "String", "date": "LocalDate", "age": "Integer"},
-    },
+        "className": "Journal",
+        "attr": {
+            "name": "String",
+            "description": "String",
+            "subject": "String",
+            "title": "String",
+            "language": "String",
+            "publisher": "User",
+            "noOfPages": "Integer",
+            "validatedBy": "Organization",
+            "visibility": "String",
+            "contributors": "List<User>",
+            "requestForAccess": "List<User>",
+        },
+    }
 ]
-
 
 if not os.path.isdir("src"):
     os.mkdir("src")
@@ -21,7 +86,7 @@ for c in classes:
     if not os.path.isdir(path):
         os.mkdir(path)
     path += "/" + c["className"]
-    packageName = "package " + "edu.bethlehem.test." + c["className"] + ";"
+    packageName = "package " + "edu.bethlehem.scinexus." + c["className"] + ";"
     generateClass.generateClass(path, c["className"], c["attr"], packageName)
     generateController.generateController(path, c["className"], c["attr"], packageName)
     generateModelAssembler.generateModelAssembler(path, c["className"], packageName)
